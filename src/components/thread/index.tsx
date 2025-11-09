423916765c39b2af8d3cce50e3e5c7eca00175d5
강민석 <minsuk4820@naver.com>
2025-11-09 15:11:05 +0900

    feat: 깃허브 아이콘 컴포넌트 unwrap

--- src/components/thread/index.tsx
+++ src/components/thread/index.tsx
@@ -341,9 +341,7 @@ export function Thread() {
                   </Button>
                 )}
               </div>
-              <div className="absolute top-2 right-4 flex items-center">
-                <OpenGitHubRepo />
-              </div>
+              <OpenGitHubRepo />
             </div>
           )}
           {chatStarted && (
